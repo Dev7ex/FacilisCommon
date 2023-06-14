4089fb8d80799a0493b66fea693ceff73fd12341
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2023-06-14 12:07:43 +0200

    Update BukkitCommonConfiguration.java

--- facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/BukkitCommonConfiguration.java
+++ facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/BukkitCommonConfiguration.java
@@ -2,7 +2,10 @@ package com.dev7ex.common.bukkit;
 
 import com.dev7ex.common.bukkit.configuration.ConfigurationProperties;
 import com.dev7ex.common.bukkit.plugin.configuration.DefaultPluginConfiguration;
+
 import org.bukkit.plugin.Plugin;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Dev7ex
@@ -11,7 +14,7 @@ import org.bukkit.plugin.Plugin;
 @ConfigurationProperties(fileName = "config.yml")
 public class BukkitCommonConfiguration extends DefaultPluginConfiguration {
 
-    public BukkitCommonConfiguration(final Plugin plugin) {
+    public BukkitCommonConfiguration(@NotNull final Plugin plugin) {
         super(plugin);
     }
 }
