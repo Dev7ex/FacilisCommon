3bea948b7fd370a9b29d643b8932c4877010cd59
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2023-06-14 12:17:32 +0200

    Update WorldRegion.java

--- facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/world/WorldRegion.java
+++ facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/world/WorldRegion.java
@@ -2,26 +2,18 @@ package com.dev7ex.common.bukkit.world;
 
 import com.dev7ex.common.bukkit.world.cuboid.Cuboid;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-
 import org.bukkit.Location;
 
 /**
  * @author Dev7ex
  * @since 19.05.2021
  */
-@Getter(AccessLevel.PUBLIC)
-public class WorldRegion {
+public interface WorldRegion {
 
-    private final Location firstLocation;
-    private final Location secondLocation;
-    private final Cuboid cuboid;
+    Location getFirstCorner();
 
-    public WorldRegion(final Location firstLocation, final Location secondLocation) {
-        this.firstLocation = firstLocation;
-        this.secondLocation = secondLocation;
-        this.cuboid = new Cuboid(firstLocation, secondLocation);
-    }
+    Location getSecondCorner();
+
+    Cuboid getCuboid();
 
 }
