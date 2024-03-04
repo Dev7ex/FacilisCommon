772ccbc49cd6073427bccb6bf15611961acb5d0d
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-04 15:39:55 +0100

    Create DatabaseProperties.java

---
+++ facilis-common-core/src/main/java/com/dev7ex/common/database/DatabaseProperties.java
@@ -0,0 +1,19 @@
+package com.dev7ex.common.database;
+
+/**
+ * @author Dev7ex
+ * @since 17.02.2024
+ */
+public interface DatabaseProperties {
+
+    String getHostName();
+
+    int getPort();
+
+    String getUserName();
+
+    String getPassword();
+
+    boolean isConnectionAllowed();
+
+}
