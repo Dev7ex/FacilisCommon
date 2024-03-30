b4c6226e7385d5cf5a86a4760bb5227b62c5f311
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-30 15:13:23 +0100

    Create CraftBukkitCommon.java

---
+++ facilis-common-craftbukkit/facilis-common-craftbukkit-core/src/main/java/com/dev7ex/common/bukkit/craftbukkit/CraftBukkitCommon.java
@@ -0,0 +1,13 @@
+package com.dev7ex.common.bukkit.craftbukkit;
+
+/**
+ * @author Dev7ex
+ * @since 21.08.2023
+ */
+public interface CraftBukkitCommon {
+
+    Object getMinecraftServer();
+
+    Object getCraftWorld(final String name);
+
+}
