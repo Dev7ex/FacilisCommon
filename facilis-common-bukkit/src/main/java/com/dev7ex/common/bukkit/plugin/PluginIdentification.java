5418cdb9184d92ac98a137575e4b3d9af5ba094d
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-30 15:14:17 +0100

    Update PluginIdentification.java

--- facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/plugin/PluginIdentification.java
+++ facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/plugin/PluginIdentification.java
@@ -12,7 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface PluginIdentification {
-
     int spigotResourceId() default 0;
 
 }
