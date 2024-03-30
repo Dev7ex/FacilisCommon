85e7df27b3473f959ec757887aa52b08c0766095
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-30 15:14:26 +0100

    Update Inventorys.java

--- facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/inventory/Inventorys.java
+++ facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/inventory/Inventorys.java
@@ -3,6 +3,7 @@ package com.dev7ex.common.bukkit.inventory;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
@@ -17,6 +18,10 @@ public class Inventorys {
             return Optional.empty();
         }
         return Optional.of(player.getOpenInventory().getTopInventory());
+    }
+
+    public static boolean isSimilar(@Nullable final Inventory firstInventory, @Nullable final Inventory secondInventory) {
+        return ((firstInventory != null) && (secondInventory != null)) && ((firstInventory.getHolder()) != (secondInventory.getHolder()));
     }
 
 }
