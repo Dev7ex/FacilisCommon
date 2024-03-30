0a580851eced9f3daadbf2be4f7dcbd129e7295c
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-30 15:14:15 +0100

    Update PluginStatisticProperties.java

--- facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/plugin/statistic/PluginStatisticProperties.java
+++ facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/plugin/statistic/PluginStatisticProperties.java
@@ -12,7 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface PluginStatisticProperties {
-
     boolean enabled() default false;
 
     int identification() default 0;
