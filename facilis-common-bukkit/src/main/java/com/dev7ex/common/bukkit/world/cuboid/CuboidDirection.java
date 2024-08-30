43c2de538901489f4422c1cf491537aad36ba5a8
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-08-30 18:02:02 +0200

    Update CuboidDirection.java

--- facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/world/cuboid/CuboidDirection.java
+++ facilis-common-bukkit/src/main/java/com/dev7ex/common/bukkit/world/cuboid/CuboidDirection.java
@@ -6,39 +6,39 @@ package com.dev7ex.common.bukkit.world.cuboid;
  */
 public enum CuboidDirection {
 
-    North,
-    East,
-    South,
-    West,
-    Up,
-    Down,
-    Horizontal,
-    Vertical,
-    Both,
-    Unknown;
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    UP,
+    DOWN,
+    HORIZONTAL,
+    VERTICAL,
+    BOTH,
+    UNKNOWN;
 
-    public CuboidDirection opposite() {
-        switch (this) {
-            case North:
-                return South;
-            case East:
-                return West;
-            case South:
-                return North;
-            case West:
-                return East;
-            case Horizontal:
-                return Vertical;
-            case Vertical:
-                return Horizontal;
-            case Up:
-                return Down;
-            case Down:
-                return Up;
-            case Both:
-                return Both;
-            default:
-                return Unknown;
-        }
+    /**
+     * Returns the opposite direction of the current direction.
+     *
+     * <p>For example, if the current direction is NORTH, this method will return SOUTH.
+     * If the current direction is HORIZONTAL, it will return VERTICAL, and vice versa.
+     * If the direction is BOTH or UNKNOWN, it returns the same direction.</p>
+     *
+     * @return The opposite direction, or the same direction if no direct opposite exists.
+     */
+    public CuboidDirection getOpposite() {
+        return switch (this) {
+            case NORTH -> SOUTH;
+            case EAST -> WEST;
+            case SOUTH -> NORTH;
+            case WEST -> EAST;
+            case UP -> DOWN;
+            case DOWN -> UP;
+            case HORIZONTAL -> VERTICAL;
+            case VERTICAL -> HORIZONTAL;
+            case BOTH -> BOTH;
+            default -> UNKNOWN;
+        };
     }
+
 }
